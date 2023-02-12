3027eafdf423b17e441d6f76c1b43f1683d9275c
gitdegsew <abebawdegsew@gmail.com>
2023-02-12 11:20:31 +0300

    connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:agino_client/presentation/dashboard/reset_sensors.dart';
 import 'package:agino_client/presentation/dashboard/statistics_one_.dart';
 import 'package:agino_client/presentation/reusable_widgets/draw_graph.dart';
+import 'package:agino_client/presentation/sensors/add_sensor.dart';
+import 'package:agino_client/presentation/sensors/fields.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -15,22 +23,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Agino Farm',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Agino Farm',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
 
-        ),
-        
-
-        // Getx routing
-        initialRoute: "/",
-        getPages: [
-          GetPage(name: "/", page: (() => StatisticsOne())),
-          GetPage(name: "/resetGDD", page: (() => ResetGDD())),
-          
-        ],
-        
-        );
+      // Getx routing
+      initialRoute: "/",
+      getPages: [
+        GetPage(name: "/", page: (() => AddField())),
+        GetPage(name: "/resetGDD", page: (() => ResetGDD())),
+      ],
+    );
   }
 }
