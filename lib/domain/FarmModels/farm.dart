0b375357bd834aa551d8789335ef36e1956cc07f
abi26anamo <abianamo282@gmail.com>
2023-02-13 12:54:44 +0300

    updated farm.dart

--- lib/domain/FarmModels/farm.dart
+++ lib/domain/FarmModels/farm.dart
@@ -1,40 +1,55 @@
 import 'dart:convert';
 
-class Farm {
-  int farmId;
-  String name;
-  String postcode;
-  String city;
-  String country;
+import 'package:agino_client/domain/FieldModel/field.dart';
 
-  Farm({
-    required this.farmId,
-    required this.name,
-    required this.postcode,
-    required this.city,
-    required this.country,
-  });
+class Farm {
+  int? farmId;
+  String? name;
+  String? latitude;
+  String? longitude;
+  String? userId;
+  List<Field>? farmfields;
+
+  Farm(
+      {required this.farmId,
+      required this.name,
+      required this.latitude,
+      required this.longitude,
+      required this.userId,
+      this.farmfields}) {
+    farmId = farmId;
+    name = name;
+    latitude = latitude;
+    longitude = longitude;
+    userId = userId;
+    farmfields = farmfields;
+  }
 
   Map<String, dynamic> toMap() {
     return {
       'farmId': farmId,
       'name': name,
-      'postcode': postcode,
-      'city': city,
-      'country': country
+      'latitude': latitude,
+      'longitude': longitude,
+      'userId': userId,
+      'farmfields': farmfields
     };
   }
 
-  factory Farm.fromMap(Map<String, dynamic> map) {
-    return Farm(
-      farmId: map['farmId'],
-      name: map['name'],
-      postcode: map['postcode'],
-      city: map['city'],
-      country: map['country'],
-    );
+  Farm.fromMap(Map<String, dynamic> json) {
+    farmId = json['farmId'];
+    name = json['name'];
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+    userId = json['userId'];
+    if (json["farmfields"] != null) {
+      farmfields = <Field>[];
+      (json['farmfields']).forEach((field) {
+        farmfields!.add(Field.fromMap(field));
+      });
+    }
   }
-  
+
   String toJson() => json.encode(toMap());
   // ignore: non_constant_identifier_names
   factory Farm.Json(String source) => Farm.fromMap(json.decode(source));
