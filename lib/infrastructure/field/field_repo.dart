c4ad6fe7504f42cf9da476cbb88ed0fca780e800
jeal <jealsabb@gmail.com>
2023-02-13 17:03:24 +0300

    added repository for field

---
+++ lib/infrastructure/field/field_repo.dart
@@ -0,0 +1,27 @@
+import 'package:agino_client/domain/FieldModel/field.dart';
+import 'package:agino_client/infrastructure/api/api_client.dart';
+import 'package:agino_client/utils/app_constants.dart';
+import 'package:get/get.dart';
+
+
+class FieldRepo extends GetxService {
+  final ApiClient apiClient;
+
+  FieldRepo({required this.apiClient});
+
+  Future<Response> getFields() async {
+    return await apiClient.getData(AppConstants.GET_FIELD_END_POINT);
+  }
+
+  Future<Response> postField(Field field) async {
+    return await apiClient.postData(AppConstants.POST_FIELD_END_POINT, field);
+  }
+
+  Future<Response> putField(Field field) async {
+    return await apiClient.putData(AppConstants.PUT_FIELD_END_POINT, field);
+  }
+
+  Future<Response> deleteField(Field field) async {
+    return await apiClient.deleteData(AppConstants.DELETE_FIELD_END_POINT);
+  }
+}
