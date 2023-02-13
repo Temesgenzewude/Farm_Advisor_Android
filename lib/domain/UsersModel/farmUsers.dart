08a47b4ae7a7bf1e0dcced127d49100241d9a70c
abi26anamo <abianamo282@gmail.com>
2023-02-13 07:52:21 +0300

    added farmUsers.dart

---
+++ lib/domain/UsersModel/farmUsers.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+class FarmUsers {
+  final int userId;
+  final int farmId;
+  final String role;
+  final String timestamp;
+  final String receivedNotification;
+ 
+
+  FarmUsers({
+    required this.userId,
+    required this.farmId,
+    required this.role,
+    required this.timestamp,
+    required this.receivedNotification,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId':userId,
+      'farmId': farmId,
+      'role': role,
+      'timestamp': timestamp,
+      'receivedNotification': receivedNotification,
+    };
+  }
+
+  factory FarmUsers.fromMap(Map<String, dynamic> map) {
+    return FarmUsers(
+      userId: map['userId'],
+      farmId: map['farmId'],
+      role: map['role'],
+      timestamp: map['timestamp'],
+      receivedNotification: map['receivedNotification'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+  factory FarmUsers.Json(String source) =>
+      FarmUsers.fromMap(json.decode(source));
+}
+
+
